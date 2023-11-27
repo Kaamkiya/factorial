bd2c01b38761f5a7b313f94efd7fdbeb26a855b8
Kaamkiya <138614660+Kaamkiya@users.noreply.github.com>
2023-11-26 20:59:05 -0500

    Add factorial in Lua

---
+++ archive/factorial.lua
@@ -0,0 +1,11 @@
+function factorial(n) 
+  if n < 2 then 
+    return 1
+  end
+  
+  return n * factorial(n - 1)
+end
+
+print("Enter a number to find the factorial of: ")
+io.read("*n")
+print(factorial(6))
